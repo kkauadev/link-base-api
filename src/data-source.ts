8c97308a4b7a6f731644aecb6e13291a63845639
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-05-17 23:06:57 -0200

    refactor: added corrects .env variables from database

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: Number(process.env.PORT),
+  host: process.env.HOST_DB,
+  port: Number(process.env.PORT_DB),
   username: process.env.USERNAME_DB,
   password: process.env.PASSWORD_DB,
-  database: "link-base",
+  database: process.env.NAME_DB,
   synchronize: true,
   logging: false,
   entities: ["src/entities/*.ts"],
--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,6 @@ app.use(linkRouter);
 
 AppDataSource.initialize().then(() => console.log("Connect to database"));
 
-app.listen(process.env.PORT_SERVER, () =>
+app.listen(process.env.PORT_SERVER ?? 3333, () =>
   console.log("running on port " + process.env.PORT_SERVER)
 );
