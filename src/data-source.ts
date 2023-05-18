2bad024698a23031940d38cc2de7320c6411339a
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-05-18 16:40:44 -0200

    refactor: resolve bugs

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,8 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   url: process.env.DATABASE_URL ?? undefined,
   type: "postgres",
-  host: process.env.HOST_DB,
-  port: 5432,
   username: "link_base_pg_user",
   password: process.env.PASSWORD_DB,
-  database: "link_base_pg",
   synchronize: false,
   logging: false,
   entities: ["src/entities/*.ts", "build/entities/*.js"],
